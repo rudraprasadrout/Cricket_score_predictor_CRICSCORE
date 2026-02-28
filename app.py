2e36f722744c9f0a7d7963a2419260bdce6e0d6b
Rudra Prasad Rout <rudraprasadrout07@gmail.com>
2026-02-28 19:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -43,4 +44,5 @@ def predict():
         return jsonify({'status': 'error', 'message': str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
